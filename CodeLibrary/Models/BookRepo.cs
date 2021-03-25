47911f5f08bf3d7f08397b3d0b786ad3161b1581
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2021-03-25 13:10:17 +0100

    Add project files.

---
+++ CodeLibrary/Models/BookRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CodeLibrary.Models
+{
+    public class BookRepo
+    {
+        public BookRepo()
+        {
+        }
+
+        public void AddBook(Book book)
+        {
+            using (IDbConnection connection = new SqlConnection(Helper.ConStr("Books")))
+            {
+                connection.Execute("InsertBook", book, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ CodeLibrary/Models/Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibrary.Models
+{
+    public static class Helper
+    {
+        public static string ConStr(string name) => ConfigurationManager.ConnectionStrings[name].ConnectionString;
+    }
+}
